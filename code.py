a843ca3e795b85f2967d5c96309741a37254d5dd
Alireza Ghafouri <alirezaghafouri98@gmail.com>
2020-11-15 11:25:57 +0330

    reading data from file & make the list of things

--- code.py
+++ code.py
@@ -3,3 +3,19 @@ class thing:
         self.weight=weight
         self.value=value
 
+things=[]
+
+# reading data from file 
+
+file= open("data.txt", "rt")
+file.readline()
+lines=file.readlines()
+for line in lines:
+    temp= line.split(",",1)
+    obj = thing ( int(temp[0]) , temp[1] )
+    things.append(obj)
+
+#for t in things:
+#    print(t.weight,"    " , t.value)
+
+
